b7c66e49a3ae8bb3bcc508b71110f4779841e6d5
apassaradar <dar.apassara@gmail.com>
2022-01-17 17:11:00 +0700

    second connect to database

--- client/src/pages/AllCourses/FoleyCath.js
+++ client/src/pages/AllCourses/FoleyCath.js
@@ -1,71 +1,164 @@
-import React from 'react';
-import Datetime from '../../components/Datetime';
+import React, { useState } from "react";
+import Axios from "axios";
+import Datetime from "../../components/Datetime";
 
+const FoleyCath = () => {
+  const [hn, setHn] = useState("");
+  const [patient_name, setPatientName] = useState("");
+  const [diagnosis, setDiagnosis] = useState("");
+  const [ward, setWard] = useState("");
+  const [unit, setUnit] = useState("");
 
-export const FoleyCath = () => {
-    return (
-        <div className="container">
-            <Datetime className="mb-20" />
-            <h1 className='info-name'>รายชื่อผู้ป่วยที่ได้ใส่ Foley Cath.</h1>
-            <div className='info-detail'>อย่างน้อย 2 ราย</div>
-            <div className="information">
-                <form action="">
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="patient_name">
-                            ชื่อผู้ป่วย :
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter name"
+  const [foleycathList, setFoleyCathList] = useState([]);
 
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="hn">HN. :</label>
-                        <input
-                            type="number"
-                            className="form-control"
-                            placeholder="Enter HN."
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="diagnosis">Diagnosis :</label>
-                        <textarea
-                            type="text"
-                            row="5"
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="ward">Ward :</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter Ward"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="unit">Unit :</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter Unit"
-                        />
-                    </div>
-                    <button class="btn btn-success">
-                        Add Patient
-                    </button>
-                </form>
-            </div>
-            <hr />
-            <div className="patients">
-                <button class="btn btn-primary" >
-                    Show Patients
-                </button>
-            </div>
-        </div>
+  const getDatas = () => {
+    Axios.get("http://localhost:3001/courses/foleycath").then((response) => {
+      setFoleyCathList(response.data);
+    });
+  };
 
+  const addData = () => {
+    Axios.post("http://localhost:3001/courses/foleycath", {
+      hn: hn,
+      patient_name: patient_name,
+      diagnosis: diagnosis,
+      ward: ward,
+      unit: unit,
+    }).then(() => {
+      setFoleyCathList([
+        ...foleycathList,
+        {
+          hn: hn,
+          patient_name: patient_name,
+          diagnosis: diagnosis,
+          ward: ward,
+          unit: unit,
+        },
+      ]);
+    });
+  };
+
+  const deleteData = (id) => {
+    Axios.delete(`http://localhost:3001/courses/foleycath/${id}`).then(
+      (response) => {
+        setFoleyCathList(
+          foleycathList.filter((val) => {
+            return val.id != id;
+          })
+        );
+      }
     );
+  };
+
+  return (
+    <div className="container">
+      <Datetime className="mb-20" />
+      <h1 className="info-name">รายชื่อผู้ป่วยที่ได้ใส่ Foley Cath.</h1>
+      <div className="info-detail">อย่างน้อย 2 ราย</div>
+      <div className="information">
+        <form action="">
+          <div className="mb-3">
+            <label className="form-label" htmlFor="hn">
+              HN. :
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Enter HN."
+              onChange={(event) => {
+                setHn(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="patient_name">
+              ชื่อผู้ป่วย :
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter name"
+              onChange={(event) => {
+                setPatientName(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="diagnosis">
+              Diagnosis :
+            </label>
+            <textarea
+              type="text"
+              row="5"
+              className="form-control"
+              onChange={(event) => {
+                setDiagnosis(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="ward">
+              Ward :
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Ward"
+              onChange={(event) => {
+                setWard(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="unit">
+              Unit :
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Unit"
+              onChange={(event) => {
+                setUnit(event.target.value);
+              }}
+            />
+          </div>
+          <button class="btn btn-success" onClick={addData}>
+            Add Data
+          </button>
+        </form>
+      </div>
+      <hr />
+      <div className="datas">
+        <button class="btn btn-primary" onClick={getDatas}>
+          Show Datas
+        </button>
+        {foleycathList.map((val, key) => {
+          return (
+            <div className="data card">
+              <div className="data-card-body text-left">
+                <p className="data-card-text">HN. : {val.hn}</p>
+                <p className="data-card-text">
+                  ชื่อผู้ป่วย : {val.patient_name}
+                </p>
+                <p className="data-card-text">Diagnosis : {val.diagnosis}</p>
+                <p className="data-card-text">Ward : {val.ward}</p>
+                <p className="data-card-text">Unit : {val.unit}</p>
+                <button
+                  btn
+                  btn-danger
+                  onClick={() => {
+                    deleteData(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
+export default FoleyCath;
--- client/src/pages/AllCourses/HelpObserveMinor.js
+++ client/src/pages/AllCourses/HelpObserveMinor.js
@@ -1,69 +1,168 @@
-import React from 'react';
-import Datetime from '../../components/Datetime';
+import React, { useState } from "react";
+import Axios from "axios";
+import Datetime from "../../components/Datetime";
 
+const HelpObserveMinor = () => {
+  const [hn, setHn] = useState("");
+  const [patient_name, setPatientName] = useState("");
+  const [diagnosis, setDiagnosis] = useState("");
+  const [ward, setWard] = useState("");
+  const [unit, setUnit] = useState("");
 
-export const HelpObserveMinor = () => {
-    return (
-        <div className="container">
-            <Datetime className="mb-20" />
-            <h1 className='info-name'>รายชื่อผู้ป่วยที่ได้เข้าสังเกตหรือช่วยการผ่าตัดเล็ก</h1>
-            <div className='info-detail'>อย่างน้อย 2 ราย (ไม่รวมเย็บแผล)</div>
-            <div className="information">
-                <form action="">
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="patient_name">
-                            ชื่อผู้ป่วย :
-                        </label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter name"
+  const [helpobserveminorList, setHelpObserveMinorList] = useState([]);
 
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="hn">HN. :</label>
-                        <input
-                            type="number"
-                            className="form-control"
-                            placeholder="Enter HN."
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="diagnosis">Diagnosis :</label>
-                        <textarea
-                            type="text"
-                            row="5"
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="ward">Ward :</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter Ward"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label" htmlFor="unit">Unit :</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Enter Unit"
-                        />
-                    </div>
-                    <button class="btn btn-success">
-                        Add Patient
-                    </button>
-                </form>
-            </div>
-            <hr />
-            <div className="patients">
-                <button class="btn btn-primary" >
-                    Show Patients
-                </button>
-            </div>
-        </div>
+  const getDatas = () => {
+    Axios.get("http://localhost:3001/courses/helpobserveminor").then(
+      (response) => {
+        setHelpObserveMinorList(response.data);
+      }
     );
+  };
+
+  const addData = () => {
+    Axios.post("http://localhost:3001/courses/helpobserveminor", {
+      hn: hn,
+      patient_name: patient_name,
+      diagnosis: diagnosis,
+      ward: ward,
+      unit: unit,
+    }).then(() => {
+      setHelpObserveMinorList([
+        ...helpobserveminorList,
+        {
+          hn: hn,
+          patient_name: patient_name,
+          diagnosis: diagnosis,
+          ward: ward,
+          unit: unit,
+        },
+      ]);
+    });
+  };
+
+  const deleteData = (id) => {
+    Axios.delete(`http://localhost:3001/courses/helpobserveminor/${id}`).then(
+      (response) => {
+        setHelpObserveMinorList(
+          helpobserveminorList.filter((val) => {
+            return val.id != id;
+          })
+        );
+      }
+    );
+  };
+
+  return (
+    <div className="container">
+      <Datetime className="mb-20" />
+      <h1 className="info-name">
+        รายชื่อผู้ป่วยที่ได้เข้าสังเกตหรือช่วยการผ่าตัดเล็ก
+      </h1>
+      <div className="info-detail">อย่างน้อย 2 ราย (ไม่รวมเย็บแผล)</div>
+      <div className="information">
+        <form action="">
+          <div className="mb-3">
+            <label className="form-label" htmlFor="hn">
+              HN. :
+            </label>
+            <input
+              type="number"
+              className="form-control"
+              placeholder="Enter HN."
+              onChange={(event) => {
+                setHn(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="patient_name">
+              ชื่อผู้ป่วย :
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter name"
+              onChange={(event) => {
+                setPatientName(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="diagnosis">
+              Diagnosis :
+            </label>
+            <textarea
+              type="text"
+              row="5"
+              className="form-control"
+              onChange={(event) => {
+                setDiagnosis(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="ward">
+              Ward :
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Ward"
+              onChange={(event) => {
+                setWard(event.target.value);
+              }}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label" htmlFor="unit">
+              Unit :
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter Unit"
+              onChange={(event) => {
+                setUnit(event.target.value);
+              }}
+            />
+          </div>
+          <button class="btn btn-success" onClick={addData}>
+            Add Data
+          </button>
+        </form>
+      </div>
+      <hr />
+      <div className="datas">
+        <button class="btn btn-primary" onClick={getDatas}>
+          Show Datas
+        </button>
+        {helpobserveminorList.map((val, key) => {
+          return (
+            <div className="data card">
+              <div className="data-card-body text-left">
+                <p className="data-card-text">HN. : {val.hn}</p>
+                <p className="data-card-text">
+                  ชื่อผู้ป่วย : {val.patient_name}
+                </p>
+                <p className="data-card-text">Diagnosis : {val.diagnosis}</p>
+                <p className="data-card-text">Ward : {val.ward}</p>
+                <p className="data-card-text">Unit : {val.unit}</p>
+                <button
+                  btn
+                  btn-danger
+                  onClick={() => {
+                    deleteData(val.id);
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
+
+export default HelpObserveMinor;
