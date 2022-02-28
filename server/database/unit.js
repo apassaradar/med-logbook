d6e75ba69324b179834ad443f37faaec58ed55a5
dream2382 <artid_sudjoi@cmu.ac.th>
2022-03-01 02:51:40 +0700

    feature/all_database

---
+++ server/database/unit.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "",
+     database: "medstudentlogbook",
+  });
+
+db.connect(() => {
+    let sql = 'CREATE TABLE unit(unitID int AUTO_INCREMENT, unit int,  PRIMARY KEY(unitID))';
+ 
+ 
+db.query(sql, (err, result) => {
+        if(err) throw err;
+        console.log(result);
+        res.send('Database created...');
+    });
+  
+  });
